b9e4f4079bd1e946f8bd8faaf118aa30b45fd07e
DownyPrio <qhmhit@163.com>
2019-08-05 09:54:03 +0800

    activation functions added.

--- xDeepFM/__init__.py
+++ xDeepFM/__init__.py
@@ -2,4 +2,5 @@ import xDeepFM.LR_model as LR
 import xDeepFM.CIN_model as CIN
 import xDeepFM.DNN_model as DNN
 import xDeepFM.ModelSequence as Sequence
+import xDeepFM.Activation as Act_func
 import numpy as np
